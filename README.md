ca27bce1043318f3445ff3720d284476a6fa9427
Philip W <philip.whitt@sbcglobal.net>
2016-05-03 15:04:41 -0500

    Adding status badge and mysql test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
+[![Build Status](https://drone.io/github.com/fliglio/health/status.png)](https://drone.io/github.com/fliglio/health/latest)
+
 # fliglio-healthcheck
 Suite of common health checks for PHP services.
-
-
---
+++ test/Fliglio/Health/Api/MysqlCheckTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fliglio\Health\Api;
+
+use Fliglio\Health\HealthManager;
+
+class MysqlCheckTest extends \PHPUnit_Framework_TestCase { 
+
+	public function test_CheckUp() {
+		$manager = new HealthManager();
+		$manager->addCheck(new MysqlCheck('localhost', 'root', ''));
+
+		$this->assertTrue($manager->runAll()->isUp());
+	}
+
+	public function test_CheckDown() {
+		$manager = new HealthManager();
+		$manager->addCheck(new MysqlCheck('localhost', 'notauser', ''));
+
+		$this->assertTrue($manager->runAll()->isDown());
+	}
+
+}
