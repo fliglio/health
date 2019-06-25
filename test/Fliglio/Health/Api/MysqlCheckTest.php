3fb3a09ea6fb55c2454107045a8542909de3a231
Philip Whitt <philip.whitt@sbcglobal.net>
2019-06-25 14:37:16 -0500

    * Mysql localhost vs 127.0.0.1

--- test/Fliglio/Health/Api/MysqlCheckTest.php
+++ test/Fliglio/Health/Api/MysqlCheckTest.php
@@ -8,14 +8,14 @@ class MysqlCheckTest extends \PHPUnit_Framework_TestCase {
 
 	public function test_CheckUp() {
 		$manager = new HealthManager();
-		$manager->addCheck(new MysqlCheck('localhost', 'root', ''));
+		$manager->addCheck(new MysqlCheck('127.0.0.1', 'root', ''));
 
 		$this->assertTrue($manager->runAll()->isUp());
 	}
 
 	public function test_CheckDown() {
 		$manager = new HealthManager();
-		$manager->addCheck(new MysqlCheck('localhost', 'root', 'badpasswordandstuff'));
+		$manager->addCheck(new MysqlCheck('127.0.0.1', 'root', 'badpasswordandstuff'));
 
 		$this->assertTrue($manager->runAll()->isDown());
 	}
